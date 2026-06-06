408dfbb761856a56c4f945ca72402b03ddbe7bd8
javier gonzales <sv72190555@idat.pe>
2026-06-05 23:11:02 -0500

    arreglo mongo2

--- back/src/tests/api/health.test.js
+++ back/src/tests/api/health.test.js
@@ -2,7 +2,14 @@
 const request = require('supertest');
 
 jest.mock('../../database/connection', () => ({ connectDB: jest.fn() }));
-jest.mock('connect-mongo', () => ({ create: jest.fn(() => ({})) }));
+jest.mock('connect-mongo', () => ({
+  create: jest.fn(() => ({
+    on: jest.fn(),
+    get: jest.fn(),
+    set: jest.fn(),
+    destroy: jest.fn(),
+  }))
+}));
 
 const app = require('../../app');
 
